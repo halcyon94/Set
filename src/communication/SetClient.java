605dd7d4b68e09b3f8bf1a52010ef3c88ce22bdf
halcyon94 <resuto@cooper.edu>
2015-02-17 00:05:18 -0500

    Create SetClient.java

---
+++ src/communication/SetClient.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package communication;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+/**
+ *
+ * @author Skorpion
+ */
+public class SetClient {
+    public static PrintWriter OUT;
+    
+    public static void main(String[] args) throws IOException{
+        Connect();
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while((s=in.readLine())!=null && (s.length() !=0)){
+            //from gui, strings go here in stead of s 
+            OUT.println(s);
+            OUT.flush();
+        }
+    }
+    
+    public static void Connect()
+    {
+            try
+            {
+                   final int PORT=3000;
+                   final String HOST="LocalHost";
+                   Socket SOCK=new Socket(HOST,PORT);
+                   ClientSideThread cst=new ClientSideThread(SOCK);
+                   OUT=new PrintWriter(SOCK.getOutputStream());
+                   Thread X=new Thread(cst);
+                   X.start();
+
+            }
+            catch(Exception X)
+            {
+                    System.err.print(X);
+                    System.exit(0);
+            }
+    }
+}
+
